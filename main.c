8df1ae953bba0b5acc5b5d100187ff314bb186a3
OumarD10 <odiarra1006@gmail.com>
2026-05-20 17:32:19 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,32 +2,39 @@
 #include "plateau.h"
 
 int main(void){
-int plateau[LI][COL];
-int player = PLAYER_1;
-int colonne;
-int reussite;
+    char plateau[LI][COL];
+    int player = joueur1;
+    int colonne;
+    int reussite;
 
-init_plateau(plateau);
+    init_plateau(plateau);
 
-while (1){
-print_plateau(plateau);
+    while (1){
+        print_plateau(plateau);
 
-printf("Joueur %d, choisissez une colonne: ", player);
-scanf("%d", &colonne);
+        printf("Joueur %d, choisissez une colonne: ", player);
+        
+        if (scanf("%d", &colonne) != 1) {
+            printf("Saisie invalide (chiffre uniquement).\n");
+            while(getchar() != '\n'); 
+            continue;
+        }
+        
+        while(getchar() != '\n'); 
 
-reussite = insert_piece(plateau, colonne, player);
+        reussite = insert_piece(plateau, colonne, player);
 
-if (reussite == 0){
-printf("Invalide. reessayez.\n");
-continue;
-}
+        if (reussite == 0){
+            printf("Coup invalide. Reessayez.\n");
+            continue;
+        }
 
-if (player == joueur1){
-
-player = joueur2;
-else
-player = joueur2;
-}
-}
-return 0;
+        if (player == joueur1){
+            player = joueur2;
+        } else {
+            player = joueur1;
+        }
+    }
+    
+    return 0;
 }
