ade1b2e382b3f554a3ad4fb02112727521c481a8
OumarD10 <odiarra1006@gmail.com>
2026-05-22 22:15:42 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
-#include "plateau.h"
+
 
 int main(void){
     char plateau[LI][COL];
     int player = joueur1;
     int colonne;
     int reussite;
+    int p_ligne; 
+    int p_col;
+    int taille_zone;
+    int sens;
 
     init_plateau(plateau);
 
@@ -23,18 +26,64 @@ int main(void){
         while(getchar() != '\n'); 
 
         reussite = insert_piece(plateau, colonne, player);
-
         if (reussite == 0){
             printf("Coup invalide. Reessayez.\n");
             continue;
         }
+
+        
+        int rotation_ok = 0;
+
+        if (rand() % 2 == 0) {
+            taille_zone = 3;
+        } else {
+            taille_zone = 5;
+        }
+
+        while (rotation_ok == 0) {
+            print_plateau(plateau); 
+            printf("ON VA FAIRE PIVOTER CE PLATEAU 😈😈 ! Vous devez faire une rotation de taille : %dx%d\n", taille_zone, taille_zone);
+            printf("Joueur %d, entrez la ligne et la colonne du PIVOT (ex: 3 4): ", player);
+            if (scanf("%d %d", &p_ligne, &p_col) != 2) {
+                while(getchar() != '\n');
+                continue;
+            }
+
+            printf("Entrez le sens (1 pour Horaire, 0 pour Anti-horaire): ");
+            if (scanf("%d", &sens) != 1) {
+                while(getchar() != '\n');
+                continue;
+            }
+            while(getchar() != '\n'); 
+
+            int c_ligne = p_ligne - 1;
+            int c_col = p_col - 1;
+
+            if (zone_valide(c_ligne, c_col, taille_zone)) {
+                rotation_zone(plateau, c_ligne, c_col, taille_zone, sens);
+                rotation_ok = 1; 
+                
+                appliquer_gravite(plateau);
+            } else {
+                printf(" Zone invalide avec un pivot pour du %dx%d ! Ça depasse du plateau. Reessayez.\n", taille_zone, taille_zone);
+            }
+        }
+
+        if (check_win(plateau, player)) {
+            print_plateau(plateau);
+            printf("FIN DE LA PARTIE ! Le joueur %d a gagne la partie !!!!!! BRAVO !👏👏👏👏 \n", player);
+            break; 
+        }
+
 
         if (player == joueur1){
             player = joueur2;
         } else {
             player = joueur1;
         }
-    }
+
     
-    return 0;
+    
+}
+return 0;
 }
